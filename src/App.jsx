90fc37895ce6b358091728a6e3c4b1af376f707b
Avishek Sandhu <sandhu.avishek69@gmail.com>
2025-09-15 18:52:34 +0530

    routes working

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    
   )
 }
 
--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,1 +1,10 @@
-i
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>navbar op com</div>
+
+  )
+}
+
+export default navbar
