c02365fbe1ad657334ce8e6c23536c2eeaa74f5f
Avishek Sandhu <sandhu.avishek69@gmail.com>
2025-10-31 23:36:46 +0530

    Initial commit with Tailwind setup and Home page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import Navbar from './pages/Navbar.jsx';
+import Navbar from './pages/Navbar.jsx'
 
-
-function Allroutes() {
+function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </>
-  );
+  )
 }
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Allroutes />
-    </Router>
-  );
-}
 export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+
 const Home = () => {
   return (
-    
-    <div>this is Home</div>
+   <>
+
+<div className="flex items-center justify-center h-screen relative">
+  <h1 class="text-5xl font-bold text-blue-600 relative bottom-[9rem] right-[-14rem]">Welcome to FixMyCity</h1>
+  
+  <div className="relative bottom-[4rem] left-[-15rem]">
+    <h3 className='text-xl mb-5'>Report an Issue</h3>
+    <button className="bg-blue-600 text-white px-6 py-3 rounded-xl text-lg font-semibold hover:bg-blue-700 transition-all duration-300 mr-4">CITIZEN</button>
+    <button  className="bg-blue-600 text-white px-6 py-3 rounded-xl text-lg font-semibold hover:bg-blue-700 transition-all duration-300 mr-4">DEPARTMENTAL</button>
+    <button  className="bg-blue-600 text-white px-6 py-3 rounded-xl text-lg font-semibold hover:bg-blue-700 transition-all duration-300 mr-4">ADMIN</button>
+  </div>
+</div>
+    </>
 
   )
 }
