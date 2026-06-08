43292e67b54871f73f3e7d9e01454be29a6e34b9
Avishek Sandhu <sandhu.avishek69@gmail.com>
2026-06-09 02:33:42 +0530

    fixed cors 2

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://fixmycity-1-5wbk.onrender.com",
-      "http://localhost:5173",
+      "https://fixmycity-qi5p.onrender.com",
+      "http://localhost:5174",
     ],
     credentials: true,
   })
